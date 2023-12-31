fb8c10a918c86e9023efa9d6bef8a154657c5d0e
ElliotLuque <elliotluque@gmail.com>
2023-12-31 06:25:12 +0100

    add cli arguments

--- csv-cli.py
+++ csv-cli.py
@@ -1,8 +1,24 @@
 import sys
 import csv
+import argparse
 
-input = sys.argv[1]
-t_chars = {
+parser = argparse.ArgumentParser(description='Prints csv file as table in console')
+parser.add_argument('file', metavar='file', type=str, help='csv file to print')
+parser.add_argument('--direction', metavar='direction', type=str, help='direction of padding')
+parser.add_argument('--color', metavar='color', type=str, help='color of table elements')
+parser.add_argument('--delimiter', metavar='delimiter', type=str, help='delimiter of csv file')
+parser.add_argument('--bold', metavar='bold', type=bool, help='bold headers')
+parser.add_argument('--indexed', action='store_true', help='add index column')
+args = parser.parse_args()
+
+ALIGNMENT = args.direction if args.direction else "right"
+COLOR = args.color if args.color else "reset"
+DELIMITER = args.delimiter if args.delimiter else ";"
+BOLD = args.bold if args.bold else False
+INDEXED = args.indexed if args.indexed else False
+
+input = args.file
+table_chars = {
 	"top_left": "┌",
 	"top_right": "┐",
 	"bottom_left": "└",
@@ -15,60 +31,69 @@ t_chars = {
 	"cross_left": "├",
 	"cross_right": "┤",
 }
+table_styles = {
+	"yellow": "\033[1;33m{cell}\033[0m",
+	"red": "\033[1;31m{cell}\033[0m",
+	"green": "\033[1;32m{cell}\033[0m",
+	"blue": "\033[1;34m{cell}\033[0m",
+	"bold": "\033[1m{cell}\033[0m",
+	"reset": "\033[0m{cell}\033[0m"
+}
+
 
 table = []
 
 def table_header(header_row):
 	row = header_row['row']
 	weights = header_row['weights']
-	str = f"{t_chars['top_left']}"
+	str = f"{table_chars['top_left']}"
 
 	# First line
 	for i in range(len(row)):
-		str += f"{t_chars['horizontal'] * (weights[i] + 2)}"
+		str += f"{table_chars['horizontal'] * (weights[i] + 2)}"
 		if i < len(row) - 1:
-			str += f"{t_chars['cross_top']}"
+			str += f"{table_chars['cross_top']}"
 		else:
-			str += f"{t_chars['top_right']}\n"
+			str += f"{table_chars['top_right']}\n"
 
 	# Second line
-	str += f"{t_chars['vertical']}"
+	str += f"{table_chars['vertical']}"
 	for i in range(len(row)):
-		str += f" {row[i]} "
+		str += f" {stylize(row[i], table_styles, 'bold' if BOLD else 'reset')} "
 		if i < len(row):
-			str += f"{t_chars['vertical']}"
+			str += f"{table_chars['vertical']}"
 	
 	# Third line
-	str += f"\n{t_chars['cross_left']}"
+	str += f"\n{table_chars['cross_left']}"
 	for i in range(len(row)):
-		str += f"{t_chars['horizontal'] * (weights[i] + 2)}"
+		str += f"{table_chars['horizontal'] * (weights[i] + 2)}"
 		if i < len(row) - 1:
-			str += f"{t_chars['cross']}"
+			str += f"{table_chars['cross']}"
 		else:
-			str += f"{t_chars['cross_right']}"
+			str += f"{table_chars['cross_right']}"
 
 	return str
 
 def table_row(row):
-	str = f"{t_chars['vertical']}"
+	str = f"{table_chars['vertical']}"
 	for cell in row:
-		str += f" {cell} {t_chars['vertical']}"
+		str += f" {stylize(cell, table_styles, COLOR)} {table_chars['vertical']}"
 	return str
 
 def last_row(header_weights):
-	str = f"{t_chars['bottom_left']}"
+	str = f"{table_chars['bottom_left']}"
 	for i in range(len(header_weights)):
-		str += f"{t_chars['horizontal'] * (header_weights[i] + 2)}"
+		str += f"{table_chars['horizontal'] * (header_weights[i] + 2)}"
 		if i < len(header_weights) - 1:
-			str += f"{t_chars['cross_bottom']}"
+			str += f"{table_chars['cross_bottom']}"
 		else:
-			str += f"{t_chars['bottom_right']}"
+			str += f"{table_chars['bottom_right']}"
 	return str
 
 def cell_padding(table):
 	for i in range(len(table)):
 		for j in range(len(table[i])):
-			table[i][j] = pad(table[i][j], "right", " ", max_col_length(table, j) - len(table[i][j]))
+			table[i][j] = pad(table[i][j], ALIGNMENT, " ", max_col_length(table, j) - len(table[i][j]))
 
 def print_table(table):
 	for i in range(len(table)):
@@ -90,6 +115,9 @@ def max_col_length(table, col_number):
 			max = len(table[i][col_number]) 
 	return max			
 
+def stylize(str, collection,style):
+	return collection[style].format(cell=str)
+
 def pad(source_string, dir, char, pad):
 	if dir == "left":
 		str = char * pad
@@ -106,13 +134,20 @@ def pad(source_string, dir, char, pad):
 try:
 	row_count = 0
 
-	with open(input, 'r') as file:
-		reader = csv.reader(file, delimiter=';')
+	with open(input, 'r', encoding='utf-8') as file:
+		reader = csv.reader(file, delimiter=DELIMITER)
 		for row in reader:
 			if not row:
 				continue
 			table.append(row)
 	
+	if INDEXED:
+		for i in range(len(table)):
+			if i == 0:
+				table[i].insert(0, "(index)")
+			else:
+				table[i].insert(0, str(i))
+
 	cell_padding(table)
 	print_table(table)
 
