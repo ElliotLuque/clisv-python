efbd4bf07d37f7422ad5d5402fdcfbe63ef9fdf8
ElliotLuque <elliotluque@gmail.com>
2023-12-31 16:47:33 +0100

    add colors

--- table_styles.py
+++ table_styles.py
@@ -74,6 +74,8 @@ ANSI_CODES = {
 	"red": "\033[1;31m{cell}\033[0m",
 	"green": "\033[1;32m{cell}\033[0m",
 	"blue": "\033[1;34m{cell}\033[0m",
+	"magenta": "\033[1;35m{cell}\033[0m",
+	"cyan": "\033[1;36m{cell}\033[0m",
 	"bold": "\033[1m{cell}\033[0m",
 	"reset": "\033[0m{cell}\033[0m"
 }
