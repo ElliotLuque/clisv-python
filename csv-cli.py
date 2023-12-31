d886f7d48ab9dc2b7192da230294e5b0bf47c010
ElliotLuque <elliotluque@gmail.com>
2023-12-31 05:25:07 +0100

    fix table, refactor print methods

--- csv-cli.py
+++ csv-cli.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 input = sys.argv[1]
 t_chars = {
@@ -15,60 +16,79 @@ t_chars = {
 	"cross_right": "┤",
 }
 
-table_header = []
-table_rows = []
+table = []
 
-def build_table_header(header):
+def table_header(header_row):
+	row = header_row['row']
+	weights = header_row['weights']
 	str = f"{t_chars['top_left']}"
 
 	# First line
-	for i in range(len(header)):
-		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
-		if i < len(header) - 1:
+	for i in range(len(row)):
+		str += f"{t_chars['horizontal'] * (weights[i] + 2)}"
+		if i < len(row) - 1:
 			str += f"{t_chars['cross_top']}"
 		else:
 			str += f"{t_chars['top_right']}\n"
-			
+
 	# Second line
 	str += f"{t_chars['vertical']}"
-	for i in range(len(header)):
-		str += f" {header[i]} "
-		if i < len(header):
-			str += f"{t_chars['vertical']}"		
+	for i in range(len(row)):
+		str += f" {row[i]} "
+		if i < len(row):
+			str += f"{t_chars['vertical']}"
 	
 	# Third line
 	str += f"\n{t_chars['cross_left']}"
-	for i in range(len(header)):
-		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
-		if i < len(header) - 1:
+	for i in range(len(row)):
+		str += f"{t_chars['horizontal'] * (weights[i] + 2)}"
+		if i < len(row) - 1:
 			str += f"{t_chars['cross']}"
 		else:
 			str += f"{t_chars['cross_right']}"
 
 	return str
-	
-def build_table_row(row):
+
+def table_row(row):
 	str = f"{t_chars['vertical']}"
 	for cell in row:
-		str += f"\033[1;33m {cell} \033[0m{t_chars['vertical']}"
+		str += f" {cell} {t_chars['vertical']}"
 	return str
 
-def build_table_row_last(header):
+def last_row(header_weights):
 	str = f"{t_chars['bottom_left']}"
-	for i in range(len(header)):
-		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
-		if i < len(header) - 1:
+	for i in range(len(header_weights)):
+		str += f"{t_chars['horizontal'] * (header_weights[i] + 2)}"
+		if i < len(header_weights) - 1:
 			str += f"{t_chars['cross_bottom']}"
 		else:
 			str += f"{t_chars['bottom_right']}"
 	return str
 
-def build_table(header, rows):
-	print(build_table_header(header))
-	
-	for row in rows:
-		print(build_table_row(row))
-	print(build_table_row_last(header))
+def cell_padding(table):
+	for i in range(len(table)):
+		for j in range(len(table[i])):
+			table[i][j] = pad(table[i][j], "right", " ", max_col_length(table, j) - len(table[i][j]))
+
+def print_table(table):
+	for i in range(len(table)):
+		header_row = {
+			"row": table[i],
+			"weights": [max_col_length(table, j) for j in range(len(table[i]))]
+		}
+		if i == 0:
+			print(table_header(header_row))
+		else:
+			print(table_row(table[i]))
+
+	print(last_row(header_row['weights']))
+		
+def max_col_length(table, col_number):
+	max = 0
+	for i in range(len(table)):
+		if max < len(table[i][col_number]):
+			max = len(table[i][col_number]) 
+	return max			
 
 def pad(source_string, dir, char, pad):
 	if dir == "left":
@@ -86,20 +106,16 @@ def pad(source_string, dir, char, pad):
 try:
 	row_count = 0
 
-	with open(input, "r") as file:
-		for line in file:
-			line = line.strip()
-
-			if not line:
+	with open(input, 'r') as file:
+		reader = csv.reader(file, delimiter=';')
+		for row in reader:
+			if not row:
 				continue
+			table.append(row)
+	
+	cell_padding(table)
+	print_table(table)
 
-			if row_count == 0:
-				table_header = line.split(";")
-			else:
-				table_rows.append(line.split(";"))
-			row_count += 1
-
-	build_table(table_header, table_rows)
 
 except IOError:
 	print(f"File {input} not found")
