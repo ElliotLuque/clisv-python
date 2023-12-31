e99ff1b36cbe191e6b243ce70d7a19b04bed3f1e
ElliotLuque <elliotluque@gmail.com>
2023-12-31 04:00:57 +0100

    add rows function, refactor names

--- csv-cli.py
+++ csv-cli.py
@@ -1,8 +1,6 @@
 import sys
 
 input = sys.argv[1]
-table_headers = []
-
 t_chars = {
 	"top_left": "┌",
 	"top_right": "┐",
@@ -17,49 +15,78 @@ t_chars = {
 	"cross_right": "┤",
 }
 
-def build_table_headers(headers):
+table_header = []
+table_rows = []
+
+def build_table_header(header):
 	str = f"{t_chars['top_left']}"
 
 	# First line
-	for i in range(len(headers)):
-		str += f"{t_chars['horizontal'] * (len(headers[i]) + 2)}"
-		if i < len(headers) - 1:
+	for i in range(len(header)):
+		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
+		if i < len(header) - 1:
 			str += f"{t_chars['cross_top']}"
 		else:
 			str += f"{t_chars['top_right']}\n"
 			
 	# Second line
 	str += f"{t_chars['vertical']}"
-	for i in range(len(headers)):
-		str += f" {headers[i]} "
-		if i < len(headers):
+	for i in range(len(header)):
+		str += f" {header[i]} "
+		if i < len(header):
 			str += f"{t_chars['vertical']}"		
 	
 	# Third line
 	str += f"\n{t_chars['cross_left']}"
-	for i in range(len(headers)):
-		str += f"{t_chars['horizontal'] * (len(headers[i]) + 2)}"
-		if i < len(headers) - 1:
+	for i in range(len(header)):
+		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
+		if i < len(header) - 1:
 			str += f"{t_chars['cross']}"
 		else:
-			str += f"{t_chars['cross_right']}\n"
+			str += f"{t_chars['cross_right']}"
 
 	return str
 	
-def build_table_rows(rows):
-	pass
+def build_table_row(row):
+	str = f"{t_chars['vertical']}"
+	for cell in row:
+		str += f"\033[1;32m {cell} \033[0m {t_chars['vertical']}"
+	return str
+
+def build_table_row_last(header):
+	str = f"{t_chars['bottom_left']}"
+	for i in range(len(header)):
+		str += f"{t_chars['horizontal'] * (len(header[i]) + 2)}"
+		if i < len(header) - 1:
+			str += f"{t_chars['cross_bottom']}"
+		else:
+			str += f"{t_chars['bottom_right']}"
+	return str
+
+def build_table(header, rows):
+	print(build_table_header(header))
+	
+	for row in rows:
+		print(build_table_row(row))
+	print(build_table_row_last(header))
 
 try:
 	row_count = 0
 
 	with open(input, "r") as file:
 		for line in file:
+			line = line.strip()
+
+			if not line:
+				continue
+
 			if row_count == 0:
-				line = line.strip()
-				table_headers = line.split(";")
+				table_header = line.split(";")
+			else:
+				table_rows.append(line.split(";"))
 			row_count += 1
 
-	print(build_table_headers(table_headers))
+	build_table(table_header, table_rows)
 
 except IOError:
 	print(f"File {input} not found")
